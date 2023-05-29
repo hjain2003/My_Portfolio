93909a051235bf2b76488645e0b6529c17f7472e
Harsh <hjain_be21@thapar.edu>
2023-05-29 22:41:03 +0530

    added custom cursor

--- src/components/Mainpage/Mainpage.js
+++ src/components/Mainpage/Mainpage.js
@@ -1,38 +1,41 @@
 import React from 'react'
 import './Mainpage.css'
+import { Cursor } from 'react-creative-cursor';
+import 'react-creative-cursor/dist/styles.css';
 
 const Mainpage = () => {
   return (
     <>
-    <div className="main_page_container">
+    <Cursor isGelly={true} />
+      <div className="main_page_container" data-cursor-size="40px" data-cursor-color="#61dbfb" >
         <div className="mainpage_content">
-        <span className='title'>Hello! I am</span>
-        <h1>HARSH JAIN</h1>
-        <h2>Driven By Passion,Fueled By Code</h2>
-        <br /><br />
-        <div className="mainContent_coffee">  
-        <p>Welcome to my portfolio! I'm a passionate Full-Stack Developer dedicated<br/> to crafting functional websites and applications that solves real-life<br/> problems.Additionally, I'm an ML/AI enthusiast, currently expanding <br/> my knowledge in this exciting field to unlock the potential of <br/>intelligent technologies.</p>
+          <span className='title'>Hello! I am</span>
+          <h1>HARSH JAIN</h1>
+          <h2>Driven By Passion,Fueled By Code</h2>
+          <br /><br />
+          <div className="mainContent_coffee">
+            <p>Welcome to my portfolio! I'm a passionate Full-Stack Developer dedicated<br /> to crafting functional websites and applications that solves real-life<br /> problems.Additionally, I'm an ML/AI enthusiast, currently expanding <br /> my knowledge in this exciting field to unlock the potential of <br />intelligent technologies.</p>
 
-        <div id="container">
-  <div class="steam" id="steam1"> </div>
-  <div class="steam" id="steam2"> </div>
-  <div class="steam" id="steam3"> </div>
-  <div class="steam" id="steam4"> </div>
+            <div id="container">
+              <div class="steam" id="steam1"> </div>
+              <div class="steam" id="steam2"> </div>
+              <div class="steam" id="steam3"> </div>
+              <div class="steam" id="steam4"> </div>
 
-  <div id="cup">
-    <div id="cup-body">
-      <div id="cup-shade"></div>
-    </div>
-    <div id="cup-handle"></div>
-  </div>
+              <div id="cup">
+                <div id="cup-body">
+                  <div id="cup-shade"></div>
+                </div>
+                <div id="cup-handle"></div>
+              </div>
 
-  <div id="saucer"></div>
+              <div id="saucer"></div>
 
-  <div id="shadow"></div>
-</div>
+              <div id="shadow"></div>
+            </div>
+          </div>
         </div>
-    </div>
-    </div>
+      </div>
     </>
   )
 }
